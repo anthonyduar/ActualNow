1e82c31e2dea2952a89d349996d5111cca92b792
anthonyduar <anthonyraduarte@gmail.com>
2026-08-27 14:05:57 -0400

    posición de buscar y linea separadora

--- app/components/ClientElements.tsx
+++ app/components/ClientElements.tsx
@@ -62,7 +62,8 @@ export function SearchButton() {
 
   return (
     <>
-      <div className="ml-auto flex items-center group border-l pl-6 md:mr-16 md:pl-10 border-gray-200">
+      <div className="ml-auto flex items-center md:mr-16">
+      <div className="h-4 w-[1px] bg-gray-300 ml-2 mr-10" />
         <button
           onClick={() => setSearchOpen(!searchOpen)}
           className="flex items-center gap-2 font-bold hover:text-sky-500 transition uppercase text-gray-700"
